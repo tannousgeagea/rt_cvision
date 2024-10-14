1933d112089d44318e56982a7c67e82bef775c7e
tannousgeagea <tannous.geagea@wasteant.com>
2024-10-14 09:48:39 +0200

    start required process at entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,30 @@
 #!/bin/bash
 set -e
 
-sudo -E supervisord -n -c /etc/supervisord.conf
+# Start supervisord as a background process
+sudo -E supervisord -c /etc/supervisord.conf &
 
+# Sleep for a few seconds to ensure supervisord starts properly
+echo "Waiting for supervisord to initialize..."
+sleep 5
+
+# Start the required services
+/bin/bash -c 'supervisorctl start data_acquisition:data_acquisition_server \
+                                segmentation:segments_comuting_server \
+                                impurity:impurity_comuting_server \
+                                visualizer:visualizer_comuting_server'
+
+echo "Sleeping for 10 seconds..."
+sleep 10
+
+/bin/bash -c 'supervisorctl start segmentation:segments_data_reader \
+                                    impurity:impurity_data_reader \
+                                    visualizer:visualizer_data_reader'
+
+echo "Sleeping for 10 seconds..."
+sleep 10
+
+/bin/bash -c 'supervisorctl start data_acquisition:data_acquisition_endpoint'
+
+# Keep the container alive by tailing a log file or by sleeping indefinitely
+tail -f /dev/null
